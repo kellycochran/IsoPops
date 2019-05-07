b8ca8ccfdadf7a589d72a83f8496a99407e4fd05
Kelly Cochran <39920537+kellycochran@users.noreply.github.com>
2019-05-06 21:42:30 -0400

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste("Welcome to IsoDB version ",
-                              utils::packageVersion("IsoDB"), ".", sep = ""))
+  packageStartupMessage(paste("Welcome to IsoPops version ",
+                              utils::packageVersion("IsoPops"), ".", sep = ""))
 }
